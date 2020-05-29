3ef80ef35feff766a83acd4a681ffaf239eb5839
harshpatel44 <harshgp44@gmail.com>
2020-05-29 01:14:33 -0300

    UserController- admin route added and admin.html created

--- src/main/java/CSCI5308/GroupFormationTool/AccessControl/IUserController.java
+++ src/main/java/CSCI5308/GroupFormationTool/AccessControl/IUserController.java
@@ -1,7 +1,9 @@
 package CSCI5308.GroupFormationTool.AccessControl;
 
+import org.springframework.web.servlet.ModelAndView;
+
 public interface IUserController {
 
     public boolean createUser();
-
+    public ModelAndView adminPage();
 }
--- src/main/java/CSCI5308/GroupFormationTool/Controller/UserController.java
+++ src/main/java/CSCI5308/GroupFormationTool/Controller/UserController.java
@@ -5,9 +5,11 @@ import CSCI5308.GroupFormationTool.AccessControl.IUserService;
 import CSCI5308.GroupFormationTool.Model.User;
 import CSCI5308.GroupFormationTool.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class UserController implements IUserController {
@@ -20,5 +22,13 @@ public class UserController implements IUserController {
         User user = new User();
         user.setFirstName("padmesh");
         return true;
+    }
+    
+    @GetMapping("/admin")
+    @Override
+    public ModelAndView adminPage() {
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("admin");
+    return mv;
     }
 }
