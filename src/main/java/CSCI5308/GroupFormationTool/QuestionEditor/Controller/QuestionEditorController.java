e95a2f6f92428217996a36c6fc44610b0475cec6
Harsh Patel <hr402607@dal.ca>
2020-06-14 13:30:16 -0300

    question editor all pages flow created

--- src/main/java/CSCI5308/GroupFormationTool/QuestionEditor/Controller/QuestionEditorController.java
+++ src/main/java/CSCI5308/GroupFormationTool/QuestionEditor/Controller/QuestionEditorController.java
@@ -9,10 +9,43 @@ import org.springframework.web.servlet.ModelAndView;
 public class QuestionEditorController {
 
     @RequestMapping("/addQuestion")
-    public ModelAndView createQuestion(@RequestParam(name="courseId") String courseId, @RequestParam(name="userId") String userId){
-        ModelAndView mv = new ModelAndView("questionEditor");
+    public ModelAndView addQuestion(@RequestParam(name="courseId") String courseId, @RequestParam(name="userId") String userId){
+        ModelAndView mv = new ModelAndView("questionEditorHome");
         mv.addObject("courseId",courseId);
         mv.addObject("userId",userId);
         return mv;
     }
+
+    @RequestMapping("/createQuestion")
+    public ModelAndView createQuestion(@RequestParam(name="courseId") String courseId, @RequestParam(name="userId") String userId){
+        ModelAndView mv = new ModelAndView("questionEditorCreateQuestion");
+        mv.addObject("courseId",courseId);
+        mv.addObject("userId",userId);
+        return mv;
+    }
+
+    @RequestMapping("/createOption")
+    public ModelAndView createOption(@RequestParam(name="courseId") String courseId, @RequestParam(name="userId") String userId){
+        ModelAndView mv = new ModelAndView("questionEditorOption");
+        mv.addObject("courseId",courseId);
+        mv.addObject("userId",userId);
+        return mv;
+    }
+
+    @RequestMapping("/questionPreview")
+    public ModelAndView questionPreview(@RequestParam(name="courseId") String courseId, @RequestParam(name="userId") String userId){
+        ModelAndView mv = new ModelAndView("questionEditorPreview");
+        mv.addObject("courseId",courseId);
+        mv.addObject("userId",userId);
+        return mv;
+    }
+
+    @RequestMapping("/questionEditorFinish")
+    public ModelAndView questionFinish(@RequestParam(name="courseId") String courseId, @RequestParam(name="userId") String userId){
+        ModelAndView mv = new ModelAndView("questionEditorFinish");
+        mv.addObject("courseId",courseId);
+        mv.addObject("userId",userId);
+        return mv;
+    }
+
 }
