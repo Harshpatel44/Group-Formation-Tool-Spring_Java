f02dba60b3d44b73f2cbbc27e7584b5db117c61a
Arjun <ar630689@dal.ca>
2020-06-18 22:30:19 -0300

    Release branch

--- src/main/java/CSCI5308/GroupFormationTool/ApplicationConstants.java
+++ src/main/java/CSCI5308/GroupFormationTool/ApplicationConstants.java
@@ -2,12 +2,12 @@ package CSCI5308.GroupFormationTool;
 
 public class ApplicationConstants {
 
-	public static final String applicationUrl = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_9_DEVINT?useSSL=false&serverTimezone=UTC";
-	public static final String dbUser = "CSCI5308_9_DEVINT_USER";
-	public static final String dbPassword = "CSCI5308_9_DEVINT_9017";
+	public static final String applicationUrl = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_9_PRODUCTION?useSSL=false&serverTimezone=UTC";
+	public static final String dbUser = "CSCI5308_9_PRODUCTION_USER";
+	public static final String dbPassword = "CSCI5308_9_PRODUCTION_9914";
 	public static final String emailSender = "sdcmaster09@gmail.com";
 	public static final String emailSenderPassword = "sdc.master";
-	public static final String restLink = "https://group9-develop.herokuapp.com/";
+	public static final String restLink = "https://group9-production.herokuapp.com/";
 	public static final String updatePasswordLink = "updateNewPassword?passKey=";
 	public static final String randomKeyForLink = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789"
 			+ "abcdefghijklmnopqrstuvxyz";
